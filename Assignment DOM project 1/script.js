8efff617e4ed927834b3b650971e0c04f7d55178
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-02-27 08:19:36 +0530

    adding dom1

---
+++ Assignment DOM project 1/script.js
@@ -0,0 +1,13 @@
+const box = document.getElementById("box");
+box.innerText = "Hello, I'm a div!";
+box.style.backgroundColor="yellow";
+box.style.color="blue";
+box.style.margin="20px";
+box.style.padding="10px";
+box.style.fontSize = "18px";
+box.style.fontWeight = "bold"
+box.style.height="200px";
+box.style.width="300px";
+
+
+
