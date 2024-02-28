27fcd1a739be00518e51b91b37f8746abc597164
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-02-28 09:49:24 +0530

    updating

--- Dark Mode/script.js
+++ Dark Mode/script.js
@@ -1,10 +1,22 @@
 const bodyEle = document.getElementsByTagName("body")[0];
 const changemode = document.querySelector("#changeMode");
 const h1 = document.getElementsByTagName("h1")[0];
+let toggled = false;
 changemode.addEventListener('click',()=>{
-    bodyEle.style.backgroundColor = "black";
-    bodyEle.style.color = "white";
-    changemode.style.backgroundColor = "rgb(34,34,34)";
+    if(!toggled)
+    { 
+        bodyEle.style.backgroundColor = "black";
+        bodyEle.style.color = "white";
+        changemode.style.backgroundColor = "rgb(34,34,34)";
+        toggled = true;
+    }
+    else{
+        bodyEle.style.backgroundColor = "white";
+        bodyEle.style.color = "black";
+        changemode.style.backgroundColor = "rgb(34,34,34)";
+        
+        toggled = false;
+    }
 })
 console.log();
 
