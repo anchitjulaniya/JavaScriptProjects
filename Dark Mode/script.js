14beea56d1d345dc9925cc1a65dea96b6fe4bec6
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-02-28 10:28:29 +0530

    adding

--- Dark Mode/script.js
+++ Dark Mode/script.js
@@ -1,6 +1,7 @@
 const bodyEle = document.getElementsByTagName("body")[0];
 const changemode = document.querySelector("#changeMode");
 const h1 = document.getElementsByTagName("h1")[0];
+const circle = document.getElementById("circle")
 let toggled = false;
 changemode.addEventListener('click',()=>{
     if(!toggled)
@@ -8,17 +9,18 @@ changemode.addEventListener('click',()=>{
         bodyEle.style.backgroundColor = "black";
         bodyEle.style.color = "white";
         changemode.style.backgroundColor = "rgb(34,34,34)";
+        circle.style.left = "5px"; 
         toggled = true;
     }
     else{
         bodyEle.style.backgroundColor = "white";
         bodyEle.style.color = "black";
         changemode.style.backgroundColor = "rgb(34,34,34)";
-        changemode.style.positi = 
+        circle.style.right = "5px"; 
         toggled = false;
     }
 })
-console.log();
+
 
 
 
