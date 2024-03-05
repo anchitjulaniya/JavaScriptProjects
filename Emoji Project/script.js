3b6c975ddc833e2a5330ec0f7f590f8724421b1e
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-05 16:13:06 +0530

    Adding emoji project

---
+++ Emoji Project/script.js
@@ -0,0 +1,47 @@
+let container = document.getElementById("tablebody_container");
+let search_field = document.getElementById("search_field");
+
+let filterFunction = (value)=>{
+    let filteredData = emojiList.filter(e=>{
+        if(e.description.indexOf(value) != -1){
+            return true;
+        }
+        if(e.aliases.some(e=>e.startsWith(value))){
+            return true;
+        }
+        if(e.tags.some(e=>e.startsWith(value))){
+            return true;
+        }    
+    })
+    displayEmoji(filteredData);
+}
+
+
+function displayEmoji(value = emojiList){
+    container.innerHTML = "";
+value.forEach(e=>{
+    let new_row = document.createElement("tr");
+    let new_emoji = document.createElement("td");
+    let new_aliases = document.createElement("td");
+    let new_description = document.createElement("td");
+    new_emoji.innerText = e.emoji;
+    new_aliases.innerText = e.aliases;
+    new_description = e.description;
+
+    new_row.append(new_emoji);
+    new_row.append(new_aliases);
+    new_row.append(new_description);
+    
+    container.append(new_row);
+})
+// displayEmoji - 1
+}
+
+window.addEventListener("load", ()=>{displayEmoji(emojiList)});
+
+search_field.addEventListener('keyup',(event)=>{
+    let value = event.target.value;
+    // console.log(value);
+    // displayEmoji(value);
+    filterFunction(value)
+})
