124603ff6fdd254d92ba5a5ae728768d2e258a9f
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-07 23:06:47 +0530

    adding background in generating keyboard code

--- Generating the keyboard code/script.js
+++ Generating the keyboard code/script.js
@@ -7,7 +7,7 @@ document.onkeydown = (event) =>{
    displayBox.innerHTML = `<span>Key Pressed-></span> <span class="eventKey">${event.key}</span>`;
    keyCode.innerHTML = `<span>Key Code-></span> <span class="eventKey">${event.keyCode}</span>`;
    keyCode.style.boxShadow = "0 0 400px rgba(0,0,0,0.1)";
-
+   keyCode.style.backgroundColor = "white";
    
 
 }
