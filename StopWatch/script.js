fe1477c5ea4f3602a6e8b527e578759db32f4fef
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-07 12:53:49 +0530

    adding stopwatch

---
+++ StopWatch/script.js
@@ -0,0 +1,20 @@
+let start = document.getElementById("start");
+let stop = document.getElementById("stop");
+let reset = document.getElementById("reset");
+
+// console.log(start, stop, reset);
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let second = document.getElementById("second");
+
+// console.log(hour, minute, second);
+
+
+
+
+
+
+
+
+
+
