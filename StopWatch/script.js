b762b5ab8cbaca706b1ce377047610f18b27e458
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-07 16:23:08 +0530

    updating changes

--- StopWatch/script.js
+++ StopWatch/script.js
@@ -8,9 +8,50 @@ let minute = document.getElementById("minute");
 let second = document.getElementById("second");
 
 // console.log(hour, minute, second);
+let x = 0;
+let idSecond;
 
+let flag = false;
+let intervalFunction =  function(){
+   idSecond =  setInterval(()=>{
+    if(Number(x) <= 9 ){
+        x = "0" + x;
+        second.innerText = x;
+        x = (Number(x) + 1)%60;
+    }else{  
+        x = x + "";
+        second.innerText = x;
+        x = (Number(x) + 1)%60;
+    }
+    },100)
+console.log(idSecond);
+}
 
+start.addEventListener("click",()=>{
+   // start.setAttribute("disabled",true);
+   // start.disabled = true;
+    if(flag == false){
+        intervalFunction();
+        flag = true;
+    }   
+    
+})
 
+stop.addEventListener("click",()=>{
+    clearInterval(idSecond);
+    flag = false;
+})
+
+reset.addEventListener("click",()=>{
+    // start.setAttribute("disabled",false);
+    // console.log(idSecond);
+    x = "0";
+    clearInterval(idSecond);
+    second.innerText = "00";
+    minute.innerText = "00";
+    hour.innerText = "00";
+    flag = false;
+})
 
 
 
