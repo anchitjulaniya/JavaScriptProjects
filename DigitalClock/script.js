635aaf5ac2350f1fb13649c0714f646c6fe2fff9
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-12 12:28:53 +0530

    updating file

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -1,8 +1,23 @@
 let d = new Date();
 let changeCountry = document.getElementById("changeCountry");
+let changeTheme = document.getElementById("changeTheme");
 let countryList = 3;
 let index = 0;
 let country = document.getElementById("country");
+let body = document.body;
+
+changeTheme.addEventListener("click",()=>{
+    if(index%countryList == 0){
+        body.style.backgroundImage = "url(cool-background4.png)"  
+        body.style.backgroundSize = "cover"; 
+
+    }else if(index%countryList == 1){
+        body.style.backgroundImage = "url(cool-background3.png)"
+
+    }else if(index%countryList == 2){
+        body.style.backgroundImage = "url(cool-background2.png)";  }
+        index += 1;
+})
 
 changeCountry.addEventListener("click",()=>{
     
@@ -41,5 +56,16 @@ function time(){
 }
 time();
   
-
+let options = {
+    timeZone: 'Europe/Paris',
+    year: 'numeric',
+    month:'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  },
+  formatter = new Intl.DateTimeFormat([], options);
+ 
+console.log(formatter.format(new Date()));
 
