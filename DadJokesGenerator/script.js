61de81549fae0f1c7d5cb5b4716936280597a02d
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-13 21:43:52 +0530

    dad joke generator

---
+++ DadJokesGenerator/script.js
@@ -0,0 +1,31 @@
+let jokeButton = document.getElementById("button-14");
+
+let jokeBox = document.getElementById("jokeBox");
+
+const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
+const options = {
+    method: "GET",
+    headers: {
+      "X-Api-Key": apiKey,
+    },
+  };
+  const apiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+
+
+jokeButton.addEventListener("click",()=>{
+    
+    async function fetchJoke(){
+        try{            
+            let response = await fetch(apiURL, options);
+            let data = await response.json();
+            console.log(data[0].joke);
+            jokeBox.innerText = data[0].joke;
+
+
+        }
+        catch{
+
+        }
+    }
+    fetchJoke();
+})
