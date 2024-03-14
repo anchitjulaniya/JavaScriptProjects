a234705ef2677e32ea92a7685c80f186025c3dee
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-14 13:45:47 +0530

    add

---
+++ ImageSearchApp/script.js
@@ -0,0 +1,78 @@
+const searchButton = document.getElementById("searchButton");
+
+const SearchBox = document.getElementById("SearchBox");
+
+const imageContainer = document.getElementById("imageContainer");
+
+
+
+const accessKey = "RZEIOVfPhS7vMLkFdd2TSKGFBS4o9_FmcV1Nje3FSjw";
+let inputData = "";
+let page = 1;
+
+searchButton.addEventListener("click",()=>{
+    
+})
+
+async function imageSearch(){
+    inputData = SearchBox.value;
+    console.log(inputData);
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+    let response = await fetch(url);
+    let data = await response.json();
+
+}
+imageSearch();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
