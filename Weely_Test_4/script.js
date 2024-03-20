b61e29b800cab7e7d8fe086dc5d96dfe93d8fa46
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-20 18:59:28 +0530

    updating weekly test 4 project

--- Weely_Test_4/script.js
+++ Weely_Test_4/script.js
@@ -1,82 +1,150 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-let mobile_Container = document.getElementById("mobile_Container");
-let search = document.getElementById("inputBox");
-let searchBtn = document.getElementById("searchBtn");
-
-let searchText = "oppo";
-
-searchBtn.addEventListener("click",()=>{
-    mobile_Container.innerText = "";
-    searchText = search.value;
-    if(searchText == ""){
-        searchText = "oppo";
-    }
+    let closeBtn = document.getElementById("closeBtn");
+    let searchBtn = document.getElementById("searchBtn");
     
+    console.log(closeBtn);
+    let mobile_Container = document.getElementById("mobile_Container");
+    let showMore = document.getElementById("showMore");
+    let showBtnContainer = document.getElementById("showBtnContainer");
+    
+    let searchText = "oppo";
+
+    searchBtn.addEventListener("click",()=>{
+        mobile_Container.innerText = "";
+        searchText = search.value;
+        if(searchText == ""){
+            searchText = "oppo";
+        }
+        run();
+    })
+
+    async function run(){
+        let respose = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
+        let data = await respose.json();
+        let phone = data.data;
+
+        phone.forEach((mobile,index) => {
+            if(index< 12){
+            // console.log(mobile.brand);
+            let mobile_card = document.createElement("div");
+            mobile_card.classList.add("animate__animated","animate__bounceIn");
+            mobile_card.style.height = "520px";
+            mobile_card.style.width = "350px";
+            mobile_card.style.display = "flex";
+            mobile_card.style.flexDirection = "column";
+            mobile_card.style.alignItems = "center";
+            mobile_card.style.justifyItems = "space-evenly";
+            mobile_card.style.gap = "10px";
+            mobile_card.style.borderRadius = "20px";
+            mobile_card.style.boxShadow = "0 0 20px rgb(100,25,230)";
+    
+            mobile_card.innerHTML = `
+            <img class="mt-[20px] w-[250px]" src="${mobile.image}">
+            <h2 class="">${mobile.brand} ${mobile.phone_name}</h2>
+            <p class="px-4 text-white text-center">There are many variations of passages of available, but the majority have suffered</p>
+            <button onclick = "showDiscription();" style="text-shadow: rgb(255, 255, 255) 0px 0 8px;" class="px-4 showDetailBtn py-2 rounded-xl bg-[rgb(100,25,230)] text-white">SHOW DETAILS</button>
+            `;
+            // console.log(data.data[i].image,data.data[1].brand,data.data[i].phone_name);
+            
+                mobile_Container.append(mobile_card);  
+               
+            }  
+            
+            if(index == 12){
+                console.log("More element");
+                showMore.style.display = "flex";
+            }   
+            
+            showMore.addEventListener("click",()=>{
+                showMore.style.display = "none";
+                if(index >= 12){
+                    // console.log(mobile.brand);
+                    let mobile_card = document.createElement("div");
+                    mobile_card.classList.add("animate__animated","animate__bounceIn");
+                    mobile_card.style.height = "520px";
+                    mobile_card.style.width = "350px";
+                    mobile_card.style.display = "flex";
+                    mobile_card.style.flexDirection = "column";
+                    mobile_card.style.alignItems = "center";
+                    mobile_card.style.justifyItems = "space-evenly";
+                    mobile_card.style.gap = "10px";
+                    mobile_card.style.borderRadius = "20px";
+                    mobile_card.style.boxShadow = "0 0 20px rgb(100,25,230)";
+            
+                    mobile_card.innerHTML = `
+                    <img class="mt-[20px] w-[250px]" src="${mobile.image}">
+                    <h2 class="">${mobile.brand} ${mobile.phone_name}</h2>
+                    <p class="px-4 text-white text-center">There are many variations of passages of available, but the majority have suffered</p>
+                    <button onclick = "showDiscription();" style="text-shadow: rgb(255, 255, 255) 0px 0 8px;" class="px-4 showDetailBtn py-2 rounded-xl bg-[rgb(100,25,230)] text-white">SHOW DETAILS</button>
+                    `;
+                    // console.log(data.data[i].image,data.data[1].brand,data.data[i].phone_name);
+                    
+                        mobile_Container.append(mobile_card); 
+                        
+                    }  
+            })
+            
+            // showMore.style.display = "block";
+            
+        });
+    
+       
+    }   
+
+    // console.log("calling run");
     run();
-})
-
-async function run(){
-    let respose = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
-    let data = await respose.json();
-    let phone = data.data;
-    console.log(phone[0].slug);
-    // console.log(phone.length);
-
-    phone.forEach(mobile => {
-        // console.log(mobile.brand);
-        let mobile_card = document.createElement("div");
-        mobile_card.classList.add("animate__animated","animate__bounceIn");
-        mobile_card.style.height = "520px";
-        mobile_card.style.width = "350px";
-        mobile_card.style.display = "flex";
-        mobile_card.style.flexDirection = "column";
-        mobile_card.style.alignItems = "center";
-        mobile_card.style.justifyItems = "space-evenly";
-        mobile_card.style.gap = "10px";
-        mobile_card.style.borderRadius = "20px";
-        mobile_card.style.boxShadow = "0 0 20px rgb(100,25,230)";
- 
-        mobile_card.innerHTML = `
-        <img class="mt-[20px] w-[250px]" src="${mobile.image}">
-        <h2 class="">${mobile.brand} ${mobile.phone_name}</h2>
-        <p class="px-4 text-white text-center">There are many variations of passages of available, but the majority have suffered</p>
-        <button style="text-shadow: rgb(255, 255, 255) 0px 0 8px;" id="showDetailBtn" class="px-4 py-2 rounded-xl bg-[rgb(100,25,230)] text-white">SHOW DETAILS<button>
-        `;
-        // console.log(data.data[i].image,data.data[1].brand,data.data[i].phone_name);
-        mobile_Container.append(mobile_card);     
+    closeBtn.addEventListener("click", () => {
+        // console.log("Close button clicked");
+        showBtnContainer.classList.add("hidden");
+      
     });
- 
 
-}   
-run();
+    
 });
 
 // show button function
-function showbtn(e){
-    let showDetailBtn = document.querySelectorAll("#showDetailBtn");
-// creating div card
-    let div_container = document.createElement("div");
-    div_container.style.height = "100vh";
-    div_container.style.width = "100%";
-    div_container.style.display = "flex";
-    div_container.style.justifyContent = "center";
-    div_container.style.alignItems = "center";
-    
-    div_container.innerHTML = `
-    <div class="w-[425px] h-[570px] flex flex-col items-center gap-3">
-        <img src="Image_url" class="w-[160px] h-[212px]">
-        <h2 class="text-center">Mobile_Name</h2>
-        <h3 class="text-center">Brand_Name</h3>
-        
-        <p class="text-center">details</p>
-        <p class="text-center">Release__Date</p>
-        <button class="bg-red-600 px-4 py-2 rounded-lg ">Close</button>
-        
-    
 
-    </div>` 
-    // console.log(showDetailBtn);
+let phoneName = document.getElementById("phoneName");
+let brandName = document.getElementById("brandName");
+let phone_discription = document.getElementById("phone_discription");
+let image = document.getElementById("image");
+
+function showDiscription(e){
+    // console.log(e, "hello");
+    e = e || window.event;
+    console.log( e.target.parentNode);
+    let image = document.getElementById("image");
+    let phoneName = document.getElementById("phoneName");
+    let brandName = document.getElementById("brandName");
+    let phone_discription = document.getElementById("phone_discription");
+    image.src = ""
+
+
+    let showDetailBtn = Array.from(document.querySelectorAll(".showDetailBtn"));
+     showDetailBtn.forEach((buttons)=>{
+        // console.log(buttons);
+    })
+    
+    showBtnContainer.classList.remove("hidden");
+    // showBtnContainer.style.display = "flex" ;
+    // phoneName.innerText = ;
+    // brandName.innerText = ;
+    // phone_discription.innerText = ;
+    // image.src = `${}`
+    
 }
+
+// function close(){
+//     console.log("close button");
+//     showBtnContainer.classList.add("hidden");
+//     // showBtnContainer.style.display = "none" ;        
+// }
+
+
+
+// -----------------------------------------------------------------end
 
 
 
@@ -103,3 +171,26 @@ function showbtn(e){
     //     // console.log(data.data[i].image,data.data[1].brand,data.data[i].phone_name);
     //     mobile_Container.append(mobile_card);        
     // }
+
+
+
+
+    // // creating div card
+    // let div_container = document.createElement("div");
+    // div_container.style.height = "100vh";
+    // div_container.style.width = "100%";
+    // div_container.style.display = "flex";
+    // div_container.style.justifyContent = "center";
+    // div_container.style.alignItems = "center";
+    
+    // div_container.innerHTML = `
+    // <div class="w-[425px] h-[570px] flex flex-col items-center gap-3">
+    //     <img src="Image_url" class="w-[160px] h-[212px]">
+    //     <h2 class="text-center">Mobile_Name</h2>
+    //     <h3 class="text-center">Brand_Name</h3>
+        
+    //     <p class="text-center">details</p>
+    //     <p class="text-center">Release__Date</p>
+    //     <button class="bg-red-600 px-4 py-2 rounded-lg ">Close</button>
+    // </div>` 
+    // // console.log(showDetailBtn);
