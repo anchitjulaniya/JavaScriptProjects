eac45633b28fa51a97d3b8dd9ab8f99d43379052
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-28 17:03:35 +0530

    updating Crypto View Project

--- CryptoView_Project/script.js
+++ CryptoView_Project/script.js
@@ -1,4 +1,10 @@
 
-let response = fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr");
-let data = response.json();
-console.log(response);
+// url = let response = fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr");
+
+
+async function x(){
+    let response = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=inr");
+    let data = await response.json();
+    console.log(response);
+}
+x();
