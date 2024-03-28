9199d4603ba75f7214cf5c05575925cef5afa3ff
anchit julaniya <anchitjulaniyaofficial@gmail.com>
2024-03-28 16:53:48 +0530

    updating Crypto View Project

--- CryptoView_Project/common.js
+++ CryptoView_Project/common.js
@@ -1,9 +1,9 @@
 
 
 document.getElementById("nav_search_button").addEventListener("click",()=>{
-    window.location.href = "/search.html";
+    window.location.href = "./search.html";
 })
 
 document.getElementById("mainTitle").addEventListener("click",()=>{
-    window.location.href = "/index.html";
+    window.location.href = "./index.html";
 })
